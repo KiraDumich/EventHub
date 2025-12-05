158e7f2957ae006d313e0689a6c2a68048362945
79244 <dumichkira05@gmail.com>
2025-12-05 20:05:52 +1000

    Входной файл для работы с сервисом

--- src/server/ServerMain.java
+++ src/server/ServerMain.java
@@ -1,4 +1,25 @@
 package server;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ServerMain {
+    public static void main(String[] args) throws Exception {
+        int port = 6000;
+        DatabaseHelper db = new DatabaseHelper("jdbc:postgresql://localhost:5432/eventhub", "postgres", "ТВОЙ_ПАРОЛЬ");
+        ServerState state = new ServerState(db);
+
+        List<ClientHandler> clients = new ArrayList<>();
+        try (ServerSocket ss = new ServerSocket(port)) {
+            System.out.println("Server listening on " + port);
+            while (true) {
+                Socket s = ss.accept();
+                System.out.println("New client connected: " + s.getRemoteSocketAddress());
+                ClientHandler handler = new ClientHandler(s, state, clients);
+                clients.add(handler);
+                new Thread(handler).start();
+            }
+        }
+    }
 }
