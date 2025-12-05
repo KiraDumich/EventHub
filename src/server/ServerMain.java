8c6fa86c3ef4c116f1a2a9bf040648039c18ccfc
79244 <dumichkira05@gmail.com>
2025-12-06 09:54:41 +1000

    Правки данных для запуска

--- src/server/ServerMain.java
+++ src/server/ServerMain.java
@@ -3,12 +3,14 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.List;
+import java.sql.*;
 
 public class ServerMain {
     public static void main(String[] args) throws Exception {
         int port = 6000;
-        DatabaseHelper db = new DatabaseHelper("jdbc:postgresql://localhost:5432/eventhub", "postgres", "ТВОЙ_ПАРОЛЬ");
+        DatabaseHelper db = new DatabaseHelper("jdbc:postgresql://localhost:5432/EventHub", "postgres", "KiDu2005");
         ServerState state = new ServerState(db);
+
 
         List<ClientHandler> clients = new ArrayList<>();
         try (ServerSocket ss = new ServerSocket(port)) {
