d4e47f06e5be69186eed9dfa3781a468dba2c7ff
im-d2ana <diana.pavlova.05@inbox.ru>
2025-12-05 19:12:21 +1000

    Реализован класс Ticket

--- src/model/Ticket.java
+++ src/model/Ticket.java
@@ -1,4 +1,20 @@
 package model;
 
-public class Ticket {
+import java.io.Serializable;
+
+public abstract  class Ticket implements Serializable {
+    private final int serialNumber;
+    private static int nextSerialNumber = 1;
+
+    public Ticket() {
+        this.serialNumber = nextSerialNumber++;
+    }
+
+    public abstract double getPrice();
+    public abstract String getType();
+
+    @Override
+    public String toString() {
+        return "Ticket{" + "id=" + serialNumber + ", type=" + getType() + ", price=" + getPrice() + "}";
+    }
 }
