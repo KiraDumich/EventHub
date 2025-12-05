41f977bb7daa2e4f7fe01baf4755da57509bc055
im-d2ana <diana.pavlova.05@inbox.ru>
2025-12-05 20:28:09 +1000

    Билет типа Walkup

--- src/model/Walkup.java
+++ src/model/Walkup.java
@@ -1,4 +1,13 @@
 package model;
 
-public class Walkup {
+public class Walkup extends Ticket {
+    @Override
+    public double getPrice() {
+        return 50.0;
+    }
+
+    @Override
+    public String getType() {
+        return "Walkup";
+    }
 }
